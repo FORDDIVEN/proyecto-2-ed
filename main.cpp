941c60b686edf6c15777fe182d15603380b52cae
Luthi <forddiven@gmail.com>
2021-07-25 17:44:31 -0400

    mediciones son repeticiones

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ int main()
     double timeInsert;
     double timeAt;
 
-    MapH *hashing = new MapH(9973);
+    MapH *hashing = new MapH(120000);
     MapSV *arreglo = new MapSV();
     AVLMap *avltree = new AVLMap();
 
@@ -29,35 +29,38 @@ int main()
     int contador = 0;
     ifstream f("randomWords.txt");
     string palabra;
-    contador = 0;
     int aux;
+    int repeticiones = 1;
 
     //MIDIENDO TIEMPO INSERT PRIMERA IMPLEMENTACION - ARREGLO
-
-    clock_t start = clock();
-    contador = 0;
     f.clear();
     f.seekg(0, f.beg);
-    while (f >> palabra)
-    {
-        aux = rand() % 100;
-        pair<string, int> tupla;
-        tupla.first = palabra;
-        tupla.second = aux;
-        if (palabra.length() > 5)
-        {
-            tupla.first = palabra.substr(0, 5); //tomar desde a hasta b en mi palabra que es de 0 hasta 5 em este caso
-        }
-        arreglo->insert(tupla);
 
-        if (contador == nWords)
+    clock_t start = clock();
+    for (int i = 0; i < repeticiones; i++)
+    {
+        contador = 0;
+        while (f >> palabra)
         {
-            break;
+            aux = rand() % 100;
+            pair<string, int> tupla;
+            tupla.first = palabra;
+            tupla.second = aux;
+            if (palabra.length() > 5)
+            {
+                tupla.first = palabra.substr(0, 5); //tomar desde a hasta b en mi palabra que es de 0 hasta 5 em este caso
+            }
+            arreglo->insert(tupla);
+
+            if (contador == nWords)
+            {
+                break;
+            }
+            contador++;
         }
-        contador++;
     }
     timeInsert = ((double)clock() - start) / CLOCKS_PER_SEC;
-    printf("Tiempo Insert Arreglo: %.10f\n\n", timeInsert / (double)10);
+    printf("Tiempo Insert Arreglo: %.10f\n\n", timeInsert / (double)repeticiones);
 
     f.clear();
     f.seekg(0, f.beg);
@@ -65,29 +68,30 @@ int main()
     //MIDIENDO TIEMPO INSERT SEGUNDA IMPLEMENTACION - HASHING
 
     start = clock();
-    contador = 0;
-    f.clear();
-    f.seekg(0, f.beg);
-    while (f >> palabra)
+    for (int i = 0; i < repeticiones; i++)
     {
-        aux = rand() % 100;
-        pair<string, int> tupla;
-        tupla.first = palabra;
-        tupla.second = aux;
-        if (palabra.length() > 5)
+        contador = 0;
+        while (f >> palabra)
         {
-            tupla.first = palabra.substr(0, 5); //tomar desde a hasta b en mi palabra que es de 0 hasta 5 em este caso
-        }
-        hashing->insert(tupla);
+            aux = rand() % 100;
+            pair<string, int> tupla;
+            tupla.first = palabra;
+            tupla.second = aux;
+            if (palabra.length() > 5)
+            {
+                tupla.first = palabra.substr(0, 5); //tomar desde a hasta b en mi palabra que es de 0 hasta 5 em este caso
+            }
+            hashing->insert(tupla);
 
-        if (contador == nWords)
-        {
-            break;
+            if (contador == nWords)
+            {
+                break;
+            }
+            contador++;
         }
-        contador++;
     }
     timeInsert = ((double)clock() - start) / CLOCKS_PER_SEC;
-    printf("Tiempo Insert Hashing: %.10f\n\n", timeInsert / (double)10);
+    printf("Tiempo Insert Hashing: %.10f\n\n", timeInsert / (double)repeticiones);
 
     f.clear();
     f.seekg(0, f.beg);
@@ -95,29 +99,30 @@ int main()
     //MIDIENDO TIEMPO INSERT TERCERA IMPLEMENTACION - AVL-TREE
 
     start = clock();
-    contador = 0;
-    f.clear();
-    f.seekg(0, f.beg);
-    while (f >> palabra)
+    for (int i = 0; i < repeticiones; i++)
     {
-        aux = rand() % 100;
-        pair<string, int> tupla;
-        tupla.first = palabra;
-        tupla.second = aux;
-        if (palabra.length() > 5)
+        contador = 0;
+        while (f >> palabra)
         {
-            tupla.first = palabra.substr(0, 5); //tomar desde a hasta b en mi palabra que es de 0 hasta 5 em este caso
-        }
-        avltree->insert(tupla);
+            aux = rand() % 100;
+            pair<string, int> tupla;
+            tupla.first = palabra;
+            tupla.second = aux;
+            if (palabra.length() > 5)
+            {
+                tupla.first = palabra.substr(0, 5); //tomar desde a hasta b en mi palabra que es de 0 hasta 5 em este caso
+            }
+            avltree->insert(tupla);
 
-        if (contador == nWords)
-        {
-            break;
+            if (contador == nWords)
+            {
+                break;
+            }
+            contador++;
         }
-        contador++;
     }
     timeInsert = ((double)clock() - start) / CLOCKS_PER_SEC;
-    printf("Tiempo Insert AVL-Tree: %.10f\n", timeInsert / (double)10);
+    printf("Tiempo Insert AVL-Tree: %.10f\n", timeInsert / (double)repeticiones);
 
     f.clear();
     f.seekg(0, f.beg);
@@ -127,43 +132,50 @@ int main()
     //MIDIENDO TIEMPO METODO AT PRIMERA IMPLEMENTACION ARREGLO
 
     start = clock();
-    contador = 0;
-    while (f >> palabra)
+    for (int i = 0; i < repeticiones; i++)
     {
-
-        if (palabra.length() > 5)
+        contador = 0;
+        while (f >> palabra)
         {
-            palabra = palabra.substr(0, 5); //tomar desde a hasta b en mi palabra que es de 0 hasta 7 em este caso
-        }
-        int arreglin = arreglo->at(palabra);
 
-        if (contador == nWords)
-        {
-            break;
+            if (palabra.length() > 5)
+            {
+                palabra = palabra.substr(0, 5); //tomar desde a hasta b en mi palabra que es de 0 hasta 7 em este caso
+            }
+            int arreglin = arreglo->at(palabra);
+
+            if (contador == nWords)
+            {
+                break;
+            }
+            contador++;
         }
-        contador++;
     }
     timeAt = ((double)clock() - start) / CLOCKS_PER_SEC;
-    printf("Tiempo At Arreglo: %.10f\n\n", timeAt / (double)10);
+    printf("Tiempo At Arreglo: %.10f\n\n", timeAt / (double)repeticiones);
+
     start = clock();
-    contador = 0;
-    while (f >> palabra)
+    for (int i = 0; i < repeticiones; i++)
     {
-
-        if (palabra.length() > 5)
+        contador = 0;
+        while (f >> palabra)
         {
-            palabra = palabra.substr(0, 5); //tomar desde a hasta b en mi palabra que es de 0 hasta 7 em este caso
-        }
-        int arreglin = arreglo->at(palabra);
 
-        if (contador == nWords)
-        {
-            break;
+            if (palabra.length() > 5)
+            {
+                palabra = palabra.substr(0, 5); //tomar desde a hasta b en mi palabra que es de 0 hasta 7 em este caso
+            }
+            int arreglin = arreglo->at(palabra);
+
+            if (contador == nWords)
+            {
+                break;
+            }
+            contador++;
         }
-        contador++;
     }
     timeAt = ((double)clock() - start) / CLOCKS_PER_SEC;
-    printf("Tiempo At Arreglo cuando no encuentra elemento: %.10f\n\n", timeAt / (double)10);
+    printf("Tiempo At Arreglo cuando no encuentra elemento: %.10f\n\n", timeAt / (double)repeticiones);
 
     f.clear();
     f.seekg(0, f.beg);
@@ -171,42 +183,50 @@ int main()
     //MIDIENDO TIEMPO SEGUNDA IMPLEMENTACION HASHING METODO AT
 
     start = clock();
-    contador = 0;
-    while (f >> palabra)
+    for (int i = 0; i < repeticiones; i++)
     {
-
-        if (palabra.length() > 5)
+        contador = 0;
+        while (f >> palabra)
         {
-            palabra = palabra.substr(0, 5); //tomar desde a hasta b en mi palabra que es de 0 hasta 7 em este caso
-        }
-        int dobleH = hashing->at(palabra);
 
-        if (contador == nWords)
-        {
-            break;
+            if (palabra.length() > 5)
+            {
+                palabra = palabra.substr(0, 5); //tomar desde a hasta b en mi palabra que es de 0 hasta 7 em este caso
+            }
+            int dobleH = hashing->at(palabra);
+
+            if (contador == nWords)
+            {
+                break;
+            }
+            contador++;
         }
-        contador++;
     }
     timeAt = ((double)clock() - start) / CLOCKS_PER_SEC;
-    printf("Tiempo At Hashing: %.10f\n\n", timeAt / (double)10);
-    contador = 0;
-    while (f >> palabra)
+    printf("Tiempo At Hashing: %.10f\n\n", timeAt / (double)repeticiones);
+
+    start = clock();
+    for (int i = 0; i < repeticiones; i++)
     {
-
-        if (palabra.length() > 5)
+        contador = 0;
+        while (f >> palabra)
         {
-            palabra = palabra.substr(0, 5); //tomar desde a hasta b en mi palabra que es de 0 hasta 7 em este caso
-        }
-        int dobleH = hashing->at(palabra);
 
-        if (contador == nWords)
-        {
-            break;
+            if (palabra.length() > 5)
+            {
+                palabra = palabra.substr(0, 5); //tomar desde a hasta b en mi palabra que es de 0 hasta 7 em este caso
+            }
+            int dobleH = hashing->at(palabra);
+
+            if (contador == nWords)
+            {
+                break;
+            }
+            contador++;
         }
-        contador++;
     }
     timeAt = ((double)clock() - start) / CLOCKS_PER_SEC;
-    printf("Tiempo At Hashing cuando no encuentra elementos: %.10f\n\n", timeAt / (double)10);
+    printf("Tiempo At Hashing cuando no encuentra elementos: %.10f\n\n", timeAt / (double)repeticiones);
 
     f.clear();
     f.seekg(0, f.beg);
@@ -214,43 +234,50 @@ int main()
     //MIDIENDO TIEMPO TERCERA IMPLEMENTACION AVL-TREE METODO AT
 
     start = clock();
-    contador = 0;
-    while (f >> palabra)
+    for (int i = 0; i < repeticiones; i++)
     {
-
-        if (palabra.length() > 5)
+        contador = 0;
+        while (f >> palabra)
         {
-            palabra = palabra.substr(0, 5); //tomar desde a hasta b en mi palabra que es de 0 hasta 7 em este caso
-        }
-        int atAvl = avltree->at(palabra);
 
-        if (contador == nWords)
-        {
-            break;
+            if (palabra.length() > 5)
+            {
+                palabra = palabra.substr(0, 5); //tomar desde a hasta b en mi palabra que es de 0 hasta 7 em este caso
+            }
+            int atAvl = avltree->at(palabra);
+
+            if (contador == nWords)
+            {
+                break;
+            }
+            contador++;
         }
-        contador++;
     }
     timeAt = ((double)clock() - start) / CLOCKS_PER_SEC;
-    printf("Tiempo At AVL-Tree: %.10f\n", timeAt / (double)10);
+    printf("Tiempo At AVL-Tree: %.10f\n", timeAt / (double)repeticiones);
+
     start = clock();
-    contador = 0;
-    while (f >> palabra)
+    for (int i = 0; i < repeticiones; i++)
     {
-
-        if (palabra.length() > 5)
+        contador = 0;
+        while (f >> palabra)
         {
-            palabra = palabra.substr(0, 5); //tomar desde a hasta b en mi palabra que es de 0 hasta 7 em este caso
-        }
-        int atAvl = avltree->at(palabra);
 
-        if (contador == nWords)
-        {
-            break;
+            if (palabra.length() > 5)
+            {
+                palabra = palabra.substr(0, 5); //tomar desde a hasta b en mi palabra que es de 0 hasta 7 em este caso
+            }
+            int atAvl = avltree->at(palabra);
+
+            if (contador == nWords)
+            {
+                break;
+            }
+            contador++;
         }
-        contador++;
     }
     timeAt = ((double)clock() - start) / CLOCKS_PER_SEC;
-    printf("Tiempo At AVL-Tree cuando no encuentra elemento: %.10f\n", timeAt / (double)10);
+    printf("Tiempo At AVL-Tree cuando no encuentra elemento: %.10f\n", timeAt / (double)repeticiones);
     cout << "----------------------------------------" << endl;
 
     return 0;
